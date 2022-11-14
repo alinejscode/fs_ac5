2586af986ea13cdc1b3fa941949e228cf01a27dc
Aline Santos <100812188+alinejscode@users.noreply.github.com>
2022-11-14 13:30:59 -0300

    Add files via upload

---
+++ frontend/src/App.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import {
+  toCreate as toCreateService,
+  tolist as toListService,
+} from "./services";
+interface response {
+  texto: string;
+}
+function App() {
+  const [text, setText] = useState("");
+  const [response, setResponse] = useState({} as response);
+
+  const GetText = async () => {
+    try {
+      const data = await toListService();
+      setResponse(data);
+    } catch (erro) {
+      alert(erro);
+    }
+  };
+
+  const CreateText = (event: React.FormEvent) => {
+    event.preventDefault();
+    const Data = {
+      texto: text,
+    };
+    try {
+      toCreateService(Data);
+    } catch (erro) {
+      alert(erro);
+    }
+    GetText();
+  };
+
+  return (
+    <div className="App">
+      <form
+        onSubmit={(e) => {
+          CreateText(e);
+        }}
+      >
+        <label>Texto:</label>
+        <input onChange={(e) => setText(e.target.value)} />
+        <button type="submit">Enviar</button>
+      </form>
+      <div className="">
+        <h2>Retorno</h2>
+        <h2 className="response">{response.texto}</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/services/index.tsx
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export const toCreate = async (newText: {}) => {
+  const textData = (await axios.post("http://localhost:500/", newText)).data;
+  return textData;
+};
+
+export const tolist = async (): Promise<any> => {
+  const textData = (await axios.get("http://localhost:5000/")).data;
+  return textData;
+};
